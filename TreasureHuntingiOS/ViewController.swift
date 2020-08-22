7e408a32df22e2f9655e6d3b6a963579a4e828c6
Xiaoxiao Wu <forever.rain@hotmail.com>
2020-08-22 15:23:46 +0300

    Initial Commit

---
+++ TreasureHuntingiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TreasureHuntingiOS
+//
+//  Created by Ye Lu on 22.8.2020.
+//  Copyright © 2020 Xiaoxiao Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TreasureHuntingiOSTests/TreasureHuntingiOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  TreasureHuntingiOSTests.swift
+//  TreasureHuntingiOSTests
+//
+//  Created by Ye Lu on 22.8.2020.
+//  Copyright © 2020 Xiaoxiao Wu. All rights reserved.
+//
+
+import XCTest
+@testable import TreasureHuntingiOS
+
+class TreasureHuntingiOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
